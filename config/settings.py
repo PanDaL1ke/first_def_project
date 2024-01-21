cc4a71dbfc4e7a0ab52020c2ecca2f26c01f2ebe
PanDaL1ke <ipaanda.oo@gmail.com>
2024-01-21 19:27:37 +0500

    products common models settings admin panel

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+GLOBAL_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -31,6 +31,22 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+
+LOCAL_APPS = [
+    'common',
+    'products'
+]
+
+
+
+THIRD_PARTY_APPS = [
+    'rest_framework'
+]
+
+
+
+INSTALLED_APPS = GLOBAL_APPS + LOCAL_APPS + THIRD_PARTY_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
---
+++ products/admin.py
@@ -0,0 +1,43 @@
+from django.contrib import admin
+
+# Register your models here.
+from products.models import *
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title', 'created_at']
+    list_filter = ['created_at']
+    search_fields = ['title']
+
+
+@admin.register(Manufacturer)
+class ManufacturerAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'price', 'discount', 'created_at', 'category', 'manufacturer', 'view_count']
+
+
+@admin.register(Characteristic)
+class CharacteristicAdmin(admin.ModelAdmin):
+    list_display = ['title', 'product']
+
+
+@admin.register(CharacteristicsValue)
+class CharacteristicsValueAdmin(admin.ModelAdmin):
+    list_display = ['title', 'characterstic']
+
+
+
+@admin.register(Instruction)
+class InstructionAdmin(admin.ModelAdmin):
+    list_display = ['title', 'product']
+
+
+@admin.register(ProductImage)
+class ProductImageAdmin(admin.ModelAdmin):
+    list_display = ['id', 'product', 'image']
